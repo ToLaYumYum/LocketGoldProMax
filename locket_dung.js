ab04a21c97126eaee6d9255f03a1f384b66219c6
ToLaYumYum...! <154914919+ToLaYumYum@users.noreply.github.com>
2026-02-19 00:16:47 +0700

    Update attention message and subscription dates

--- locket_dung.js
+++ locket_dung.js
@@ -5,7 +5,7 @@ const mapping = {
 
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
 var obj = JSON.parse($response.body);
-obj.Attention = "Made by AntiDetectToLaYumYum";
+obj.Attention = "Made by AntiDetectHungChannels";
 
 var subscriptionData = {
   is_sandbox: false,
@@ -15,16 +15,16 @@ var subscriptionData = {
   expires_date: "2099-01-01T00:00:00Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2015-07-03T00:00:00Z",
-  purchase_date: "2015-07-03T00:00:00Z",
+  original_purchase_date: "2006-07-03T00:00:00Z",
+  purchase_date: "2006-07-03T00:00:00Z",
   store: "app_store"
 };
 
 var entitlementData = {
   grace_period_expires_date: null,
-  purchase_date: "2015-07-03T00:00:00Z",
+  purchase_date: "2006-07-03T00:00:00Z",
   product_identifier: "com.locket.Locket.premium.yearly",
-  expires_date: "2099-01-01T01:04:17Z"
+  expires_date: "2099-12-18T01:04:17Z"
 };
 
 const match = Object.keys(mapping).find(e => ua.includes(e));
